9c261e14eecb2291f18d6008ac26b5a2134b6d88
KARL NASSAR <kgnassar01@gmail.com>
2021-01-20 23:27:47 +0200

    demo auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,17 +22,37 @@ function Login() {
             .catch(error => alert(error.message))
     }
 
+    const signInDemo = () => {
+        dispatch(
+            login({
+                displayName: "test",
+                email: "test@test.test",
+                photoUrl: "https://codahosted.io/docs/IZn3UNbEOU/blobs/bl-137mEqTBzf/8e04b9feef4481efc0616766fb5547e099f3b5a7ecf18c5d6e1b03bbacdb9c9d74ac9f8cc0b5457d5ea1700a7cd5f866942616013c01cff01c2641e5ce293e4f9733859c5858dc1c3f1738355e8591ffd8fca9311e1e2283bb8ea449f5307c52c0dec951"
+            })
+        )
+        
+    }
+
     return (
         <div className="login">
             <div className="login__container">
                 <img src="https://static.dezeen.com/uploads/2020/10/gmail-google-logo-rebrand-workspace-design_dezeen_2364_col_0.jpg" alt="gmail logo"/>
 
                 <Button 
+                    className="login__button"
                     variant="contained" 
                     color="primary"
                     onClick={signIn}
                 >
                     Login
+                </Button>
+
+                <Button 
+                    variant="contained" 
+                    color="secondary"
+                    onClick={signInDemo}
+                >
+                    Demo Login
                 </Button>
             </div>
         </div>
